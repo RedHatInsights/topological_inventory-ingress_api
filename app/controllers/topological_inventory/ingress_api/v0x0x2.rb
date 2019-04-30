712841f198ceb3a2b5bd279d3ce48e84a54dd5f6
Ladislav Smola <lsmola@redhat.com>
2019-04-30 12:33:54 +0200

    Correctly generate openapi json

--- app/controllers/topological_inventory/ingress_api/v0x0x2.rb
+++ app/controllers/topological_inventory/ingress_api/v0x0x2.rb
@@ -3,7 +3,7 @@ module TopologicalInventory
     module V0x0x2
       class RootController < ApplicationController
         def openapi
-          render :json => Api::Docs["0.0.2"].content.to_json
+          render :json => TopologicalInventory::IngressApi::Docs["0.0"].content.to_json
         end
       end
       class InventoryController < IngressApi::V0::InventoryController; end
