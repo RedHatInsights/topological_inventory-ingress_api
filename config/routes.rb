724c6aca4eee5f869d3e5e0876f6f8a051d58ee6
Ladislav Smola <lsmola@redhat.com>
2019-04-30 10:17:09 +0200

    Change routes according to topo API

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
+  # Disable PUT for now since rails sends these :update and they aren't really the same thing.
+  def put(*_args); end
+
+  routing_helper = ManageIQ::API::Common::Routing.new(self)
+  prefix         = "ingress_api"
+  # if ENV["PATH_PREFIX"].present? && ENV["APP_NAME"].present?
+  #   prefix = File.join(ENV["PATH_PREFIX"], ENV["APP_NAME"]).gsub(/^\/+|\/+$/, "")
+  # end
+
   namespace :topological_inventory do
-    namespace :ingress_api do
+    scope :as => :ingress_api, :module => "ingress_api", :path => prefix do
       namespace :v0x0x2, :path => "0.0.2" do
-        resources :schemas, :only => [:index]
+        get "/openapi.json", :to => "root#openapi"
         post "/inventory" => "inventory#save_inventory"
       end
     end
